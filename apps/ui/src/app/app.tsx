3879cde3b507c459240f30af31f3125adf9f2c5a
Mahdi Chaabane <mahdi02ch@gmail.com>
2024-04-12 10:45:52 +0100

    first commit

---
+++ apps/ui/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+import { User } from '@teamflow/domain';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="ui" />
+    </div>
+  );
+}
+
+export default App;
---
+++ libs/domain/src/lib/domain.ts
@@ -0,0 +1,8 @@
+export function domain(): string {
+  return 'domain';
+}
+
+export type User = {
+  id: number;
+  name: string;
+};
