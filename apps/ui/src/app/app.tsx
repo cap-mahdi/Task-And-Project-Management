1fd0c74abef3c12921869867f829699c003c331e
FkihMehdi <123999605+FkihMehdi@users.noreply.github.com>
2024-04-17 16:07:17 +0200

    create navbar and footer (#14)

---
+++ apps/ui/src/app/app.tsx
@@ -0,0 +1,10 @@
+import styles from './app.module.css';
+
+// import NxWelcome from './nx-welcome';
+import { User } from '@teamflow/domain';
+
+export function App() {
+  return <div>{/* <NxWelcome title="ui" /> */}</div>;
+}
+
+export default App;
