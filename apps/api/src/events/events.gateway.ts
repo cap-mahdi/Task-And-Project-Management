98b0345aed4b2932ff9b34ef593ba7b2024480db
Med Amine Gdoura <medaminegdoura@gmail.com>
2024-04-23 23:59:34 +0100

    Swagger + GraphQL serve + Websocket server setup

---
+++ apps/api/src/events/events.gateway.ts
@@ -0,0 +1,38 @@
+import { OnModuleInit } from '@nestjs/common';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Observable, of } from 'rxjs';
+import { Server } from 'socket.io';
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+  onModuleInit() {
+    //3
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log(socket.connected);
+    });
+  }
+  // You can listen to this event
+  // Client can send message to me by using the message key/event name
+  @SubscribeMessage('message')
+  newMessage(
+    @MessageBody()
+    data: any
+  ): Observable<WsResponse<any>> {
+    //1
+    console.log('Message is receieved from the client');
+    console.log(data);
+    return of({ event: 'message', data: 'Returned from the server' });
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,15 +3,40 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+
+  //Swagger Configuration
+  const config = new DocumentBuilder() //1
+    .setTitle('Task Mangment App')
+    .setDescription('The Task Mangment App Api documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      // Enable Bearer Auth here
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth'
+    )
+
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
