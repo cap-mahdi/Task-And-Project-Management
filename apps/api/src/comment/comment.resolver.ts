9bea0cb3b3fb261789a771e72f9c644b7695c19a
Mehdi <mohamedmehdi.khelil@insat.ucar.tn>
2024-05-26 15:09:46 +0100

    added comment structure and modified landing page text

--- apps/api/src/comment/comment.resolver.ts
+++ apps/api/src/comment/comment.resolver.ts
@@ -1,10 +1,11 @@
 
 import { UseGuards } from "@nestjs/common";
-import { Resolver, Query, Args, ResolveField, Parent } from "@nestjs/graphql";
+import { Resolver, Query, Args, ResolveField, Parent, Mutation } from "@nestjs/graphql";
 import { GraphQLAuthGaurd } from '../auth/guards/gql-auth-guard';
 import { GetUserGQL } from "../auth/decorators/gql-user.decorator";
 import { CommentSchema, UserSchema } from "../entities";
 import { CommentService } from "./comment.service";
+import { CreateCommentInput, EditCommentInput } from "../graphql";
 @Resolver('Comment')
 @UseGuards(GraphQLAuthGaurd)
 export class CommentResolver {
@@ -13,12 +14,34 @@ export class CommentResolver {
         private readonly taskService: TaskService
     ) { }
 
+    @Mutation('createComment')
+    async createComment(
+        @Args('input') { content, taskId }: CreateCommentInput,
+        @GetUserGQL() user: UserSchema
+    ): Promise<CommentSchema> {
+        return this.commentService.createComment(taskId, content, user);
+    }
 
-    
+    @Mutation('deleteComment')
+    async deleteComment(
+        @Args('commentId') commentId: string,
+        @GetUserGQL() user: UserSchema
+    ): Promise<CommentSchema> {
+        return this.commentService.deleteComment(commentId, user);
+    }
+
+    @Mutation('updateComment')
+    async updateComment(
+        @Args('id') commentId: string,
+        @Args('input') { content }: EditCommentInput,
+        @GetUserGQL() user: UserSchema
+    ): Promise<CommentSchema> {
+        return this.commentService.editComment(commentId, content, user);
+    }
+
     @Query('comments')
     async comments(
         @Args('taskId') taskId: string,
-        @GetUserGQL() user: UserSchema
     ): Promise<CommentSchema[]> {
         return this.commentService.findCommentsByTaskId(taskId);
     }
