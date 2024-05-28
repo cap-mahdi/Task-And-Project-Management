4abf4dc25b06fc20a58e25a9a9a4accbfdae21c1
mahdich02 <70388243+cap-mahdi@users.noreply.github.com>
2024-05-28 08:37:19 +0100

    handle notifications in page (#99)

--- apps/ui/src/pages/notification/Notification.tsx
+++ apps/ui/src/pages/notification/Notification.tsx
@@ -1,95 +1,22 @@
 import React from 'react';
 import { NotificationItem } from '../../components/notifications';
 import { Action, UserRole } from '../../__generated__/graphql';
-import { Box, Typography } from '@mui/material';
-
-const notif = {
-  url: 'workspace/d3d4e0c3-b105-4f81-ac2d-0c053d09458c/project/15b2f276-5cf9-47d0-900b-9596ca59a4ae',
-  type: 'project',
-  id: '1',
-  actor: {
-    id: '1',
-    name: 'John Doe',
-    email: 'john@gmail.com',
-    avatar: 'https://i.pravatar.cc/300',
-    password: 'password',
-    createdAt: new Date(),
-    role: UserRole.ADMIN,
-    userWorkspaces: [],
-    userProjects: [],
-    userRooms: [],
-    userTasks: [],
-    createdWorkspaces: [],
-    createdProjects: [],
-  },
-  recipient: {
-    id: '1',
-    name: 'John Doe',
-    email: 'john@gmail.com',
-    avatar: 'https://i.pravatar.cc/300',
-    password: 'password',
-    createdAt: new Date(),
-    role: UserRole.ADMIN,
-    userWorkspaces: [],
-    userProjects: [],
-    userRooms: [],
-    userTasks: [],
-    createdWorkspaces: [],
-    createdProjects: [],
-  },
-  createdAt: new Date(),
-  project: {
-    rooms: [],
-    userProjects: [],
-    milestones: [],
-    id: '1',
-    name: 'Project 1',
-    description: 'Project 1 description',
-    workspace: {
-      id: '1',
-      name: 'Workspace 1',
-      description: 'Workspace 1 description',
-      createdAt: new Date(),
-      userWorkspaces: [],
-      projects: [],
-      creator: {
-        id: '1',
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        avatar: 'https://i.pravatar.cc/300',
-        password: 'password',
-        createdAt: new Date(),
-        role: UserRole.ADMIN,
-        userWorkspaces: [],
-        userProjects: [],
-        userRooms: [],
-        userTasks: [],
-        createdWorkspaces: [],
-        createdProjects: [],
-      },
-    },
-    createdAt: new Date(),
-    creator: {
-      id: '1',
-      name: 'John Doe',
-      email: 'john@gmail.com',
-      avatar: 'https://i.pravatar.cc/300',
-      password: 'password',
-      createdAt: new Date(),
-      role: UserRole.ADMIN,
-      userWorkspaces: [],
-      userProjects: [],
-      userRooms: [],
-      userTasks: [],
-      createdWorkspaces: [],
-      createdProjects: [],
-    },
-  },
-  action: Action.REMOVE,
-  read: true,
-};
+import { Typography } from '@mui/material';
+import useNotificationContext from '../../context/useNotificationsContext';
 
 export function Notification(props) {
+  const [{ notifications }, setNotificationState] = useNotificationContext();
+  const markAsRead = (id: string) => {
+    const clickedNotif = notifications.find((n) => n.id === id);
+    if (clickedNotif?.read) return;
+    setNotificationState((prev) => ({
+      ...prev,
+      unreadCount: prev.unreadCount - 1,
+      notifications: prev.notifications.map((n) =>
+        n.id === id ? { ...n, read: true } : n
+      ),
+    }));
+  };
   return (
     <>
       <Typography
@@ -102,10 +29,14 @@ export function Notification(props) {
       >
         Notifications
       </Typography>
-      <NotificationItem notification={notif} />
-      <NotificationItem notification={notif} />
-      <NotificationItem notification={notif} />
-      <NotificationItem notification={notif} />
+      {notifications.map((notification, index) => (
+        <NotificationItem
+          key={index}
+          notification={notification}
+          markAsRead={markAsRead}
+          onClose={() => {}}
+        />
+      ))}
     </>
   );
 }
